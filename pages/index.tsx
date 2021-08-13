8c9fc0d03ddf70755663e0041d393b43827fea51
v_vryyu <v_vryyu@tencent.com>
2021-08-13 23:15:36 +0800

    feat: del index date

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,15 +17,11 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({ id, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
           </li>
           ))}
         </ul>
