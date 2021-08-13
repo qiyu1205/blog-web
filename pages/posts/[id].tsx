cd61f0608a2293259093070d3ccd301fe839e50a
v_vryyu <v_vryyu@tencent.com>
2021-08-13 18:08:02 +0800

    style: post content

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -11,6 +11,7 @@ import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 import {
   TableWrapper,
+  Content,
 } from './styled'
 
 export default function Post({ postData }) {
@@ -59,12 +60,11 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <article>
+      <Content>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
-        {/* <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} /> */}
         <ReactMarkdown
           remarkPlugins={[gfm]}
           rehypePlugins={[rehypeRaw]}
@@ -73,7 +73,7 @@ export default function Post({ postData }) {
         >
           {removeEmbededTag(postData.contentHtml)}
         </ReactMarkdown>
-      </article>
+      </Content>
     </Layout>
   )
 }
--- pages/posts/styled.ts
+++ pages/posts/styled.ts
@@ -5,3 +5,27 @@ export const TableWrapper = styled.p`
   justify-content: center;
   margin: 2rem auto;
 `
+
+export const Content = styled.article`
+  max-width: 58rem;
+  margin: 0 auto;
+
+  h2 {
+    margin: 2.5rem 0 1.25rem;
+    font-size: 2rem;
+  }
+
+  h3 {
+    margin: 1.875rem 0 1.25rem;
+    font-size: 1.5rem;
+  }
+
+  pre {
+    font-size: 90%;
+    margin: 2rem 0;
+    border-radius: 0.4rem;
+    padding: 1rem !important;
+    line-height: 1.5;
+    background-color: rgb(40, 44, 52) !important;
+  }
+`
