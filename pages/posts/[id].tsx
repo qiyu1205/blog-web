1c12e7696359b40c073ffe6a0ecd39c0c2b1efe0
v_vryyu <v_vryyu@tencent.com>
2021-08-14 11:02:24 +0800

    feat: 使用 tocbot 生成文章目录

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -11,70 +11,92 @@ import Date from '../../components/date'
 import {
   TableWrapper,
   Content,
+  Menu,
 } from './styled'
+import tocbot from 'tocbot'
+import { PureComponent } from 'react';
 
-export default function Post({ postData }) {
-  const customMarkdownComponents = {
-    code({ node, inline, className, children, ...props }: any) {
-      const match = /language-(\w+)/.exec(className || '')
-      return !inline && match ? (
-        <SyntaxHighlighter
-          style={atomOneDark}
-          language={match[1]}
-          PreTag="div"
-          {...props}
-        >
-          {children.toString().replace(/\n$/, '')}
-        </SyntaxHighlighter>
-      ) : (
-        <code className={className} {...props}>
-          {children.toString()}
-        </code>
-      )
-    },
-    table({ node, inline, className, children, ...props }: any) {
-      return (
-        <TableWrapper>
-          <table {...props}>{children}</table>
-        </TableWrapper>
-      )
-    },
-    h2({ node, inline, className, children, ...props }: any) {
-      return (
-        <h2 {...props} id={children ? children[0] : ''}>
-          {children}
-        </h2>
-      )
-    },
-    h3({ node, inline, className, children, ...props }: any) {
-      return (
-        <h3 {...props} id={children ? children[0] : ''}>
-          {children}
-        </h3>
-      )
-    },
+interface Props {
+  postData?: any,
+}
+interface State {}
+const customMarkdownComponents = {
+  code({ node, inline, className, children, ...props }: any) {
+    const match = /language-(\w+)/.exec(className || '')
+    return !inline && match ? (
+      <SyntaxHighlighter
+        style={atomOneDark}
+        language={match[1]}
+        PreTag="div"
+        {...props}
+      >
+        {children.toString().replace(/\n$/, '')}
+      </SyntaxHighlighter>
+    ) : (
+      <code className={className} {...props}>
+        {children.toString()}
+      </code>
+    )
+  },
+  table({ node, inline, className, children, ...props }: any) {
+    return (
+      <TableWrapper>
+        <table {...props}>{children}</table>
+      </TableWrapper>
+    )
+  },
+  h2({ node, inline, className, children, ...props }: any) {
+    return (
+      <h2 {...props} id={children ? children[0] : ''}>
+        {children}
+      </h2>
+    )
+  },
+  h3({ node, inline, className, children, ...props }: any) {
+    return (
+      <h3 {...props} id={children ? children[0] : ''}>
+        {children}
+      </h3>
+    )
+  },
+}
+export default class Post extends PureComponent<Props, State> {
+
+  componentDidMount() {
+    // init tocbot
+    tocbot.init({
+      tocSelector: '.postMenu',
+      contentSelector: '.postDetailContent',
+      headingSelector: 'h1, h2, h3',
+      hasInnerContainers: true,
+    })
   }
-  return (
-    <Layout>
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
-      <Content>
-        <h1>{postData.title}</h1>
-        <div>
-          <Date dateString={postData.date} />
-        </div>
-        <ReactMarkdown
-          remarkPlugins={[gfm]}
-          rehypePlugins={[rehypeRaw]}
-          components={customMarkdownComponents}
-          className="postDetailContent"
-        >
-          {removeEmbededTag(postData.contentHtml)}
-        </ReactMarkdown>
-      </Content>
-    </Layout>
-  )
+
+  render() {
+    const { postData } = this.props;
+    return (
+      <Layout>
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <Menu className="postMenu" />
+        <Content>
+          <h1 className="postTitle">{postData.title}</h1>
+          <div className="tagsWrap">
+            <Date dateString={postData.date} />
+          </div>
+          <ReactMarkdown
+            remarkPlugins={[gfm]}
+            rehypePlugins={[rehypeRaw]}
+            components={customMarkdownComponents}
+            className="postDetailContent"
+          >
+            {removeEmbededTag(postData.contentHtml)}
+          </ReactMarkdown>
+        </Content>
+      </Layout>
+    )
+  }
 }
 
 export async function getStaticPaths() {
--- pages/posts/styled.ts
+++ pages/posts/styled.ts
@@ -10,6 +10,17 @@ export const Content = styled.article`
   max-width: 58rem;
   margin: 20px auto;
 
+  .postTitle {
+    margin: 5rem 0 1.5rem;
+    font-size: 3rem;
+    text-align: center;
+  }
+
+  .tagsWrap {
+    margin-bottom: 4rem;
+    text-align: center;
+  }
+
   h1 {
     font-size: 2.5rem;
   }
@@ -51,5 +62,28 @@ export const Content = styled.article`
     background-color: #ededeb;
     border-radius: .25rem;
     padding: 0 .25rem;
+  }
+`
+
+export const Menu = styled.aside`
+  position: fixed;
+  top: 25rem;
+  left: 2rem;
+
+  .toc-link {
+    text-decoration: none;
+    font-size: 1rem;
+    color: rgb(40, 44, 52);
+  }
+
+  .toc-list {
+    padding-left: 1rem;
+    line-height: 2rem;
+    list-style-type: none;
+  }
+
+  .is-active-link {
+    color: #007fff;
+    font-weight: 600;
   }
 `
